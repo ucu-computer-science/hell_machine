93b86f31f5cf8805302ac85889ad2fc05d12bcef
Mykola Morgunenko <Olli25010@gmail.com>
2019-12-24 22:40:21 +0200

    add motot

--- sonar/Src/tim.c
+++ sonar/Src/tim.c
@@ -36,7 +36,7 @@ void MX_TIM4_Init(void)
   htim4.Instance = TIM4;
   htim4.Init.Prescaler = 96;
   htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim4.Init.Period = 40000;
+  htim4.Init.Period = 60000;
   htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_PWM_Init(&htim4) != HAL_OK)
